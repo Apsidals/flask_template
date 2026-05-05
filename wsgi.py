ab29a8c8c54ae5ce21779d5ba96630d655b4899e
Apsidals <126407491+Apsidals@users.noreply.github.com>
2026-05-05 18:18:41 -0400

    Add ProxyFix middleware to WSGI app
    cit deploy needs this on citmain

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from app import app
 
-if __name__ == '__main__':
+app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1)
+
+if __name__ == "__main__":
     app.run()
